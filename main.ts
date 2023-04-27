6dae175d97586234433425306c7e39317a31258c
Salomon786 <128729041+Salomon786@users.noreply.github.com>
2023-04-27 17:30:29 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,13 +4,10 @@ function Suivre_la_ligne () {
     // Trop à gauche de la ligne (il faut revenir sur la droite)
     // Trop à droite de la ligne il faut revenir sur la gauche
     if (Position_sur_la_ligne == 2) {
-        Rover.setALLRGB(Rover.colors(RoverColors.Green))
         Rover.Move(Vitesse_de_croisière)
     } else if (Position_sur_la_ligne == 4 || Position_sur_la_ligne == 6) {
-        Rover.setALLRGB(Rover.colors(RoverColors.Red))
         Rover.MotorRunDual(Vitesse_minimale, Vitesse_maximale)
     } else if (Position_sur_la_ligne == 1 || Position_sur_la_ligne == 3) {
-        Rover.setALLRGB(Rover.colors(RoverColors.Blue))
         Rover.MotorRunDual(Vitesse_maximale, Vitesse_minimale)
     }
 }
@@ -18,11 +15,30 @@ let Position_sur_la_ligne = 0
 let Vitesse_maximale = 0
 let Vitesse_minimale = 0
 let Vitesse_de_croisière = 0
-basic.showIcon(IconNames.Happy)
 Vitesse_de_croisière = 90
 Vitesse_minimale = 15
 Vitesse_maximale = 130
 let arret = 0
+for (let index = 0; index < 69; index++) {
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . # .
+        . # # # .
+        `)
+    Rover.setALLRGB(Rover.colors(RoverColors.Red))
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . # # # .
+        . . . # .
+        . # # # .
+        `)
+    Rover.setALLRGB(Rover.colors(RoverColors.Yellow))
+    basic.pause(500)
+}
 basic.forever(function () {
     Suivre_la_ligne()
 })
